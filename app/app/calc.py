92baf28061e4dac5429302cee417feb61c8b619e
Segun <segunvictor052@gmail.com>
2022-11-29 15:17:44 +0100

    Configured Docker Compose and Checks to wait for DB

---
+++ app/app/calc.py
@@ -0,0 +1,17 @@
+
+
+"""
+Calculator function
+"""
+
+
+def add(x, y):  # noqa
+
+    """add x and y and rerturn result"""
+    return x + y
+
+
+def subtract(x, y):  # noqa
+
+    """subtract x and y and return result"""
+    return y - x
